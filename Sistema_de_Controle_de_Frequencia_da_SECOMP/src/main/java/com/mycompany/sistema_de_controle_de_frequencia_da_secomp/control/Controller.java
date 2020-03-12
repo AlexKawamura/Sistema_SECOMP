ac61f0fd4b4e8137a035b015163ab8cf68f9a995
Aluno <Aluno@I202-117404>
2020-03-11 21:00:50 -0300

    Atividade arquitetura 2.1

--- Sistema_de_Controle_de_Frequencia_da_SECOMP/src/main/java/com/mycompany/sistema_de_controle_de_frequencia_da_secomp/control/Controller.java
+++ Sistema_de_Controle_de_Frequencia_da_SECOMP/src/main/java/com/mycompany/sistema_de_controle_de_frequencia_da_secomp/control/Controller.java
@@ -5,8 +5,11 @@
  */
 package com.mycompany.sistema_de_controle_de_frequencia_da_secomp.control;
 
-import com.mycompany.sistema_de_controle_de_frequencia_da_secomp.model.Lista;
+// A classe Lista não é mais utilizada
+//import com.mycompany.sistema_de_controle_de_frequencia_da_secomp.model.Lista;
 import com.mycompany.sistema_de_controle_de_frequencia_da_secomp.model.Pessoa;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -15,22 +18,79 @@ import java.util.Scanner;
  */
 public class Controller {
     
+    private static ArrayList<Pessoa> listaAlunos = new ArrayList();
+    private static Scanner scanner = new Scanner(System.in);
+    
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        Lista lista = new Lista();
-        String nome = " ";
+        String op = null;
         
-        
-        while(true){
-            System.out.println("\nInforme o nome do participante:");
-            nome = scanner.nextLine();
-            if(nome.equals("imprimir")) {
-                lista.imprimeLista();   
-                return;
+        do {
+            
+            System.out.println("\n----- Menu -----");
+            System.out.println("[1] - Inserir Aluno");
+            System.out.println("[2] - Imprimir Lista\n");
+            System.out.println("[0] - Sair\n");
+            op = scanner.nextLine();
+            
+            switch(op) {
+                case "1":
+                    addPessoa();
+                    break;
+                case "2":
+                    imprimeLista();
+                    break;
             }
-            lista.addPessoa(new Pessoa(nome));
-        }
-       
+        }while(!op.equals("0"));
+        
+//        FALTOU MENU COM FUNCIONALIDADES
+//        
+//        Scanner scanner = new Scanner(System.in);
+//        Lista lista = new Lista();
+//        String nome = " ";
+//        
+//        
+//        while(true){
+//            System.out.println("\nInforme o nome do participante:");
+//            nome = scanner.nextLine();
+//            if(nome.equals("imprimir")) {
+//                lista.imprimeLista();   
+//                return;
+//            }
+//            lista.addPessoa(new Pessoa(nome));
+//        }
     }
+    
+//  DIVISÃO DO CÓDIGO EM MÉTODOS
+    public static void addPessoa(){
+        System.out.println("\nInforme o nome do participante:");
+        String nome = scanner.nextLine();
+//        Modularização do código
+//        for (int i = 0; i < listaAlunos.size(); i++) {
+//            if (listaAlunos.get(i).equals(nome) ) {
+//                System.out.println("\nErro: Pessoa ja cadastrada!!\n");
+//                return;
+//            }
+//        }
+        if (verificaNome(nome)){
+            listaAlunos.add(new Pessoa(nome));   
+        }
+    }
+    
+    public static boolean verificaNome(String nome){
+        for (Pessoa aluno : listaAlunos){
+            if(aluno.getNomeCompleto().equals(nome)){
+                System.out.println("Nome já cadastrado!");
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    public static void imprimeLista(){
+        System.out.println("\n\nLista de participantes:\n");
+        for (Pessoa aluno : listaAlunos){
+            System.out.println(aluno.getNomeCompleto());
+        }
+    } 
     
 }
